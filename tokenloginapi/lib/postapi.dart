c97e0175b644e0de8ce805d0e81691391793e032
Exalter104 <muhammadsherazkhan104@gmail.com>
2022-10-21 00:16:43 +0500

    CyberSync API:login Api for User

---
+++ tokenloginapi/lib/postapi.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController emailController = TextEditingController();
+
+  TextEditingController passwordController = TextEditingController();
+
+  void login(String email, String password) async {
+    try {
+      Response response = await post(
+          Uri.parse(
+              "https://marktestapp.pythonanywhere.com/api/auth/rider/login/"),
+          body: {
+            "email": email,
+            "password": password,
+          });
+      print(response.statusCode);
+      print(response.body);
+      if (response.statusCode == 200) {
+        print("account Login Successfully");
+      } else {
+        print("account Login failed");
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up Api'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: "email",
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                hintText: "password",
+              ),
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                login(emailController.text.toString(),
+                    passwordController.text.toString());
+              },
+              child: Container(
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Center(child: Text("Sign in")),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
